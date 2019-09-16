4be0630b7498055d28d25fec426dfb818357431d
ln <ningli06@ele.me>
2019-09-16 12:18:48 +0800

    observer

--- gupaoxueyuan/src/main/java/org/gupao/mybatis/HelloMapper.java
+++ gupaoxueyuan/src/main/java/org/gupao/mybatis/HelloMapper.java
@@ -4,11 +4,14 @@
  */
 package org.gupao.mybatis;
 
+import org.apache.ibatis.annotations.Select;
+
 /**
  * @author lining
  * @version $Id: HelloMapper.java, v 0.1 2019-09-09 5:40 PM lining Exp $
  */
 public interface HelloMapper {
 
+//    @Select("select * from helloworld where id = #{id}")
     HelloWorld selectHello(long id);
 }
