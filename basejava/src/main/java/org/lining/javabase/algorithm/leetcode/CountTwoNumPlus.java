f6f9234577759f66edd9bd58a37378d56c51f4dc
ln <ningli06@ele.me>
2019-12-05 19:22:31 +0800

    algorithm

---
+++ basejava/src/main/java/org/lining/javabase/algorithm/leetcode/CountTwoNumPlus.java
@@ -0,0 +1,75 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package org.lining.javabase.algorithm.leetcode;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author lining
+ * @version $Id: CountTwoNumPlus.java, v 0.1 2019-12-05 2:18 PM lining Exp $
+ */
+public class CountTwoNumPlus {
+
+    public static void main(String[] args) {
+        CountTwoNumPlus countTwoNumPlus = new CountTwoNumPlus();
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        Node node2 = new Node(7);
+        node2.next = new Node(6);
+        node2.next.next = new Node(7);
+        System.out.println(JSON.toJSON(node));
+        System.out.println(JSON.toJSON(node2));
+        System.out.println(JSON.toJSON(countTwoNumPlus.countTwo(node, node2)));
+    }
+    /**
+     * 两数相加
+     给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+     示例：
+     输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     输出：7 -> 0 -> 8
+     原因：342 + 465 = 807
+     */
+    public static class Node{
+        public int value;
+        public Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    //时间复杂度：O(max(m, n)) 空间复杂度：O(max(m, n)) 新列表的长度最多为 max(m,n) + 1
+    public Node countTwo(Node first, Node second){
+
+        Node result = new Node(0);
+        Node currentResult = result;
+        Node firstCurrent = first;
+        Node secondCurrent= second;
+        //进位
+        int out = 0;
+
+        while (firstCurrent != null || secondCurrent != null){
+            int firstCurrentValue = (firstCurrent == null ? 0 : firstCurrent.value);
+            int secondCurrentValue = (secondCurrent == null ? 0 : secondCurrent.value);
+            int currentSum =  firstCurrentValue + secondCurrentValue + out;
+            out = currentSum / 10;
+
+            currentResult.next = new Node(currentSum % 10);
+            firstCurrent = (firstCurrent == null ? null : firstCurrent.next);
+            secondCurrent = (secondCurrent == null ? null : secondCurrent.next);
+
+            currentResult = currentResult.next;
+            //最后一位，>0则最大位为1
+            if (out > 0){
+                currentResult.next = new Node(out);
+            }
+        }
+
+        return result.next;
+    }
+}
--- basejava/src/main/java/org/lining/javabase/algorithm/leetcode/FindTwoNumSum.java
+++ basejava/src/main/java/org/lining/javabase/algorithm/leetcode/FindTwoNumSum.java
@@ -2,7 +2,7 @@
  * Alipay.com Inc.
  * Copyright (c) 2004-2019 All Rights Reserved.
  */
-package org.lining.javabase.algorithm;
+package org.lining.javabase.algorithm.leetcode;
 
 import com.alibaba.fastjson.JSON;
 
@@ -13,9 +13,9 @@ import java.util.Map;
 
 /**
  * @author lining
- * @version $Id: LeetCode.java, v 0.1 2019-12-05 11:03 AM lining Exp $
+ * @version $Id: FindTwoNumSum.java, v 0.1 2019-12-05 11:03 AM lining Exp $
  */
-public class LeetCode {
+public class FindTwoNumSum {
 
     public static void main(String[] args) {
 
@@ -25,6 +25,7 @@ public class LeetCode {
     }
 
     /**
+     * 寻找两数之和
      给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
      你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
